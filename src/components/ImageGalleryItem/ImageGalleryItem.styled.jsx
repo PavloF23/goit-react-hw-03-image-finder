b6b1dd21f15ae824a3366fec552ce273f9590df4
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-01-20 13:48:07 +0200

    Update ImageGalleryItem.styled.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.jsx
@@ -11,6 +11,10 @@ export const Img = styled.img`
     height: 260px;
     object-fit: cover;
     transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    
+    display: block;
+    max-width: 100%;
+    height: 100%;
 
     :hover {
     transform: scale(1.03);
